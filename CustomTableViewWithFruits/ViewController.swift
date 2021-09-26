b17f026a109effbd5110226ea54a9a52c72640ca
xxpp1314 <317211206@qq.com>
2021-09-25 19:26:32 -0700

    Finishing the Assignment About Custom Table View With Fruits

--- CustomTableViewWithFruits/ViewController.swift
+++ CustomTableViewWithFruits/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    let arr = ["Apple", "Banana", "Cherry", "Grape", "Lemon", "Mango", "Orange", "Peach", "Pineapple", "Stawberry", "Watermelon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.imgView.image = UIImage(named: "Fruit\(indexPath.row+1)")
+        cell.lblImage.text = "\(arr[indexPath.row])"
+        return cell
+    }
 
 }
 
